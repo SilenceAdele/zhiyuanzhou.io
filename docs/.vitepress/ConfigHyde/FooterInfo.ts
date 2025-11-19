6ffb292aaf227f8a1613bce93743c3dcd7bb58c5
zhiyuan.zhou <zhiyuan.zhou@easystack.cn>
2025-11-19 17:30:51 +0800

    Update some Badges
    * Update some Badges
    
    Signed-off-by: zhiyuan.zhou <zzy_ahut@163.com>

--- docs/.vitepress/ConfigHyde/FooterInfo.ts
+++ docs/.vitepress/ConfigHyde/FooterInfo.ts
@@ -10,10 +10,12 @@ export const FooterInfo = {
     <a target="_blank" href="https://twikoo.js.org/" title="本站评论系统使用 Twikoo" ><img alt="Twikoo" src="https://img.shields.io/badge/Comments-Twikoo-0072F9"></a>
     
     <a target="_blank" href="https://www.algolia.com/" title="本站搜索服务使用 Algolia"><img alt="Algolia" src="https://img.shields.io/badge/Search-Algolia-3095FA?logo=Algolia"></a>
-
-    <a target="_blank" href="https://github.com/" title="本站部署服务使用 github"><img alt="github" src="https://img.shields.io/static/v1?label=Server&message=GitHub&color=FF6A00&logo=github"></a>
     
-    <a target="_blank" href="https://www.busuanzi.cc/" title="本站统计服务使用 busuanzi"><img alt="busuanzi" src="https://img.shields.io/badge/Count-Busuanzi-FF6A00?logo=busuanzi"></a>`,
+    <a target="_blank" href="https://github.com/" title="本站部署服务使用 GitHub" rel="noopener noreferrer"><img alt="github" src="https://img.shields.io/static/v1?label=Server&message=GitHub&color=181717&logo=github&logoColor=white"></a>
+    
+    <a target="_blank" href="https://www.cloudflare.com/" title="本站 CDN 服务由 Cloudflare 提供" rel="noopener noreferrer"><img alt="Cloudflare CDN" src="https://img.shields.io/badge/CDN-Cloudflare-FF6A00?logo=cloudflare"></a>
+    
+    <a target="_blank" href="https://www.busuanzi.cc/" title="本站统计服务使用 Busuanzi"><img alt="busuanzi" src="https://img.shields.io/badge/Count-Busuanzi-9b59b6?logo=busuanzi&logoColor=white"></a>`,
   ],
   theme: {
     name: `Theme By Teek@${version}-2025.10.19`,
