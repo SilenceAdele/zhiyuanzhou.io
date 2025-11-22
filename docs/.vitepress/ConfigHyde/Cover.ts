1e6c61d4dbd60f0112fbc1b2cbbb187693627a88
zhiyuan.zhou <zhiyuan.zhou@easystack.cn>
2025-11-22 20:22:20 +0800

    Rollback jsdelivr as picture cdn accelerate
    * Rollback jsdelivr as picture cdn accelerate
    
    Signed-off-by: zhiyuan.zhou <zzy_ahut@163.com>

--- docs/.vitepress/ConfigHyde/Cover.ts
+++ docs/.vitepress/ConfigHyde/Cover.ts
@@ -1,4 +1,4 @@
 // 创建文章随记默认壁纸
 export const Cover = [
-  "https://image.zhiyuanzhou.online/defalutPostBgi.jpg", 
+  "https://cdn.jsdelivr.net/gh/SilenceAdele/zhiyuanzhou.io@master/docs/public/img/imgaeBed/defalutPostBgi.jpg", 
 ];
